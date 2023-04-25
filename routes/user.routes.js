aa6fbb924c552d774cda155cc6697cf060e05571
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-04-25 15:02:40 +0530

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
+const sgMail = require("@sendgrid/mail");
 const UserRouter = express.Router();
 const { User } = require("../models/user.model");
 const redis = require("redis");
@@ -9,7 +10,6 @@ const { authentication } = require("../middlewares/authentication");
 const { Blacklist } = require("../models/blacklist.model");
 const passport = require("passport");
 require("../middlewares/google.auoth");
-
 
 UserRouter.post("/signup", async (req, res) => {
   try {
@@ -77,9 +77,74 @@ UserRouter.get(
     session: false,
   }),
   function (req, res) {
-   console.log(req.user);
+    console.log(req.user);
   }
 );
+
+// sending the password reset link to the user via email
+UserRouter.get("/forgot-password", (req, res) => {
+  try {
+    const { email } = req.query;
+    if (!email) {
+      return res.send({ msg: "Please provide email" });
+    }
+    let webid = uuid();
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // change the url to send;
+    const messages = [
+      {
+        to: `${email}`,
+        from: "akki010102@gmail.com",
+        subject: "Change Password",
+        text: "Hello from MedEasyIn",
+        html: `
+      <h1>MedEasyIn</h1>
+      <p>Click on the link below to change your password</p>
+      <b>This is a one time link to update your password</b>
+      <a href="https://medeasyin-password-update.netlify.app?webid=${webid}">Click here to reset your password</a>
+`,
+      },
+    ];
+    sgMail.send(messages).then((success, error) => {
+      if (error) {
+        res.send({
+          msg: "Something Went Wrong",
+          Error: error.response.body,
+        });
+      } else {
+        res.send({ msg: "Check your mail box to reset your password" });
+      }
+    });
+  } catch (error) {
+    res.send({ msg: error });
+  }
+});
+
+UserRouter.patch("/password-update", async (req, res) => {
+  try {
+    const { email, password, webid } = req.query;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      if (webid === undefined) {
+        return res.send({ msg: "This link is not valid" });
+      }
+      if (user.webid.includes(webid)) {
+        return res.send({ msg: "This link is already used" });
+      }
+
+      user.password = password;
+      user.webid.push(webid);
+
+      await user.save();
+      res.status(200).send({ msg: "Password updated successfully" });
+    } else {
+      res.send({ msg: "something went wrong" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send({ msg: "server error", error });
+  }
+});
 
 UserRouter.get("/refresh-token", async (req, res) => {
   try {
